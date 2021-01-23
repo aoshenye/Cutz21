93eaa477c255cafaab029d5b55c155d52a7e7530
aoshenye <aoshenye@gmail.com>
2021-01-23 23:29:02 +0000

    add post method to profile.html

--- app.py
+++ app.py
@@ -106,6 +106,12 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
+@app.route("/add_comment")
+def add_comment():
+    return render_template("add_comment.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
